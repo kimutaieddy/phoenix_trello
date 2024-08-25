eb773a6085df9baf71496b8f3f04f7d1c7dafe91
Your Name <eddykimutai@outlook.com>
2024-08-25 13:18:47 +0300

    chore: Update npm dependencies

---
+++ trello/lib/trello_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule TrelloWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use TrelloWeb, :controller` and
+  `use TrelloWeb, :live_view`.
+  """
+  use TrelloWeb, :html
+
+  embed_templates "layouts/*"
+end
