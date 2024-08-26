94d4c4ca014b2137624ce4459ef3062af5403663
Your Name <eddykimutai@outlook.com>
2024-08-26 20:45:12 +0300

    chore: Update npm dependencies and package-lock.json

--- trello/lib/trello/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Trello.Mailer do
-  use Swoosh.Mailer, otp_app: :trello
-end
--- trello/lib/trello/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Trello.Repo do
-  use Ecto.Repo,
-    otp_app: :trello,
-    adapter: Ecto.Adapters.Postgres
-end
