593a9f8c71fe1782ac6d0f1c3300ca68419e79ab
Your Name <eddykimutai@outlook.com>
2024-08-26 20:51:03 +0300

    chore: Update npm dependencies and package-lock.json

---
+++ phoenix_trello/lib/phoenix_trello_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule PhoenixTrelloWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use PhoenixTrelloWeb, :controller` and
+  `use PhoenixTrelloWeb, :live_view`.
+  """
+  use PhoenixTrelloWeb, :html
+
+  embed_templates "layouts/*"
+end
